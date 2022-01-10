ba586fe0e06596e8075c04558729392b5a6c0c6e
Hideho Arakida <x>
2022-01-10 17:07:41 +0900

    first commit

---
+++ cpp_training_00/cpp_training_00/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_training_00
+//
+//  Created by 荒木田英穂 on 2022/01/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
