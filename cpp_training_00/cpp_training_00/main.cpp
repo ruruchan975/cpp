4138ddc7fa8d50322914f6556c3a54c97a27e712
Hideho Arakida <x>
2022-01-13 00:16:26 +0900

    add test for vector class.

--- cpp_training_00/cpp_training_00/main.cpp
+++ cpp_training_00/cpp_training_00/main.cpp
@@ -6,9 +6,23 @@
 //
 
 #include <iostream>
+#include <vector>
 
+void test_vector_00(void)
+{
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    decltype(v)::iterator it = std::find(v.begin(), v.end(), 3);
+    if (it != v.end()) {
+        std::cout << *it << std::endl;
+    
+    } else {
+        std::cout << "no" << std::endl;
+        
+    }
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    test_vector_00();
     return 0;
 }
